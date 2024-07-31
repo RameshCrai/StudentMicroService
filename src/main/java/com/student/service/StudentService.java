96ec625a527b4bb837bc6e715eb052ffb211bb29
Ramesh Rai <rameshchamling277@gmail.com>
2024-07-31 18:58:30 +0545

    add eureka server address for apigateway

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -1,12 +1,11 @@
 package com.student.service;
 
-
 import com.student.payloads.StudentDto;
 
 public interface StudentService {
-	
+
 	StudentDto createStudent(StudentDto student);
-	
+
 	StudentDto getStudentByID(int id);
 
 }
