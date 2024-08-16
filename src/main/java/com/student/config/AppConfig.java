5dcef8091b2e55518da03f2d6eb30533c345edac
Ramesh Rai <rameshchamling277@gmail.com>
2024-08-16 23:12:49 +0545

    add retry request, rate limiter with jmeter

--- src/main/java/com/student/config/AppConfig.java
+++ src/main/java/com/student/config/AppConfig.java
@@ -1,24 +1,29 @@
 package com.student.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
 
 @Configuration
 public class AppConfig {
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
+
 //	@Bean
 //	public RestTemplate restTemplate() {
 //		return new RestTemplate();
 //	}
-	
+
 //	don't use this kind of hard code 
 //	@Value("${addressservice.base.url}")
 //	private String addressBaseURL;
@@ -31,5 +36,28 @@ public class AppConfig {
 //				.build();
 //				
 //	}
+
+//	retry 
+	@Bean
+	public Retry retry() {
+		RetryConfig retryConfig = RetryConfig.custom().maxAttempts(3) // Number of retry attempts
+				.waitDuration(java.time.Duration.ofSeconds(2)) // Time between retries
+				.build();
+
+		RetryRegistry retryRegistry = RetryRegistry.of(retryConfig);
+		return retryRegistry.retry("retry");
+	}
+
+	@Bean
+	public RateLimiter rateLimiter() {
+		RateLimiterConfig rateLimiterConfig = RateLimiterConfig.custom().limitForPeriod(10) // Number of allowed calls
+																							// per period
+				.limitRefreshPeriod(java.time.Duration.ofSeconds(10)) // Period duration
+				.timeoutDuration(java.time.Duration.ofMillis(500)) // Timeout for acquiring a permit
+				.build();
+
+		RateLimiterRegistry rateLimiterRegistry = RateLimiterRegistry.of(rateLimiterConfig);
+		return rateLimiterRegistry.rateLimiter("rateLimiter");
+	}
 
 }
--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -1,7 +1,5 @@
 package com.student.controller;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.student.entity.Student;
 import com.student.exceptions.ApiExceptionHandler;
 import com.student.exceptions.UserNotFoundException;
 import com.student.payloads.StudentDto;
 import com.student.service.impl.StudentServiceImpl;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class StudentController {
@@ -45,57 +43,60 @@ public class StudentController {
 //			
 //			return ResponseEntity.status(HttpStatus.OK).body(stdObj);
 //		});
-//	}
+//	
+
+	int retrycount = 1;
 
 	@GetMapping("/get-student/{id}")
-	@CircuitBreaker(name = "courseAddressBreaker", fallbackMethod = "courseAddressFallback")
+//	@CircuitBreaker(name = "courseAddressBreaker", fallbackMethod = "courseAddressFallback")
+//	@Retry(name = "retry", fallbackMethod = "retryFallback")
+	@RateLimiter(name ="rateLimiter", fallbackMethod = "rateLimiterFallback")
 	public ResponseEntity<?> getStudent(@PathVariable("id") int id) {
-	    try {
-	        StudentDto studentObj = this.serviceimpl.getStudentByID(id);
-	        if (studentObj.getStudentID() == id) {
-	            return ResponseEntity.status(HttpStatus.OK).body(studentObj);
-	        } else {
-	            String emessage = "This data does not exist";
-	            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiExceptionHandler(emessage));
-	        }
-	    } catch (UserNotFoundException ex) {
-	        ex.printStackTrace();
-	        String emessage = "An error occurred while retrieving the data";
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiExceptionHandler(emessage));
-	    }
+		try {
+			StudentDto studentObj = this.serviceimpl.getStudentByID(id);
+			if (studentObj.getStudentID() == id) {
+
+				return ResponseEntity.status(HttpStatus.OK).body(studentObj);
+			} else {
+				String emessage = "This data does not exist";
+				return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiExceptionHandler(emessage));
+			}
+		} catch (UserNotFoundException ex) {
+			ex.printStackTrace();
+			String emessage = "An error occurred while retrieving the data";
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiExceptionHandler(emessage));
+		}
 	}
 
 	// Fallback method for Circuit Breaker
-	public ResponseEntity<StudentDto> courseAddressFallback(int id, Throwable ex) {
-	    logger.info("Fallback is executed because the service is down: " + ex.getMessage());
-
-	    StudentDto studentDto = StudentDto.builder()
-	                                     .email("dummy@gmail.com")
-	                                     .fullName("Dummy K")
-	                                     .build();
-
-	    return new ResponseEntity<>(studentDto, HttpStatus.OK);
-	}
-
-
-//	@GetMapping("/get-student/{id}")
-//	@CircuitBreaker(name = "courseAddressBreaker", fallbackMethod = "courseAddressFallback")
-//	public CompletableFuture<ResponseEntity<?>> getStudentData(@PathVariable("id") int id) {
-//		return CompletableFuture.supplyAsync(() -> {
-//			try {
-//				StudentDto studentObj = this.serviceimpl.getStudentByID(id);
-//				if (studentObj.getStudentID() == id) {
-//					return ResponseEntity.status(HttpStatus.OK).body(studentObj);
-//				} else {
-//					String emessage = "This data not Exit in Real";
-//					return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiExceptionHandler(emessage));
-//				}
-//			} catch (UserNotFoundException ex) {
-//				ex.printStackTrace();
-//				String emessage = "This data not Exit in Real";
-//				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiExceptionHandler(emessage));
-//			}
-//		});
+//	public ResponseEntity<StudentDto> courseAddressFallback(int id, Throwable ex) {
+//		logger.info("Fallback is executed because the service is down: " + ex.getMessage());
+//
+//		StudentDto studentDto = StudentDto.builder().email("circuit-dummy@gmail.com").fullName("Fualt Tolerance").build();
+//
+//		return new ResponseEntity<>(studentDto, HttpStatus.OK);
 //	}
+
+	// Fallback method for Retry
+//	public ResponseEntity<StudentDto> retryFallback(int id, Throwable ex) {
+//		logger.info("Retry fallback is executed because the service failed: " + ex.getMessage());
+//
+//		logger.info("Retry count " + retrycount);
+//		retrycount++;
+//		System.out.println("retry =" + retrycount);
+//
+//		StudentDto studentDto = StudentDto.builder().email("retry-dummy@gmail.com").fullName("Retry Dummy").build();
+//
+//		return new ResponseEntity<>(studentDto, HttpStatus.OK);
+//	}
+	
+	// Fallback method for Retry
+	public ResponseEntity<StudentDto> rateLimiterFallback(int id, Throwable ex) {
+		logger.info("rate Limiter fallback is executed because the service failed: " + ex.getMessage());
+
+		StudentDto studentDto = StudentDto.builder().email("rateLimiter-dummy@gmail.com").fullName("Rate Limiter").build();
+
+		return new ResponseEntity<>(studentDto, HttpStatus.OK);
+	}
 
 }
